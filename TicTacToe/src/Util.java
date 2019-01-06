3495a72e873c80877366f5045cb59b374ed4e755
Nina1234y <43324461+Nina1234y@users.noreply.github.com>
2019-01-06 13:03:18 -0500

    Update Util.java

--- TicTacToe/src/Util.java
+++ TicTacToe/src/Util.java
@@ -1,15 +1,7 @@
 
 public class Util {
-	
 
     /**
-     * The string representing JButtons.
-     */
-	public static final String[] bt = {"$one", "$two", "$three", "$four", "$five", "$six", "$seven", "$eight", "$nine"};
-
-
-	
-	/**
      * The string representing the player.
      */
     public static final String PLAYER = "Player";
